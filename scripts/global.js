7458b549e4e40108490b805bd545f91c1bbbc4ba
jincaiyuan <cywalker@163.com>
2017-06-07 20:08:16 +0800

    1. Add some layout style 2. make a short preview of each part

--- scripts/global.js
+++ scripts/global.js
@@ -39,7 +39,71 @@ function highlightPage() {
         linkUrl = links[i].getAttribute("href");
         if(window.location.href.indexOf(linkUrl) != -1){
             addClass(links[i], "here");
+            var linkText = links[i].lastChild.nodeValue.toLowerCase();
+            document.body.setAttribute("id", linkText);
         }
     }
 }
 addLoadEvent(highlightPage);
+
+
+function moveElement(elementID, final_x, final_y, interval) {
+    var elem = document.getElementById(elementID);
+    var xpos = parseInt(elem.style.left);
+    var ypos = parseInt(elem.style.top);
+    if(xpos == final_x && ypos == final_y){
+        return true;
+    }
+    if(xpos < final_x){
+        xpos++;
+    }
+    if(xpos > final_x){
+        xpos--;
+    }
+    if(ypos < final_y){
+        ypos++;
+    }
+    if(ypos > final_y){
+        ypos--;
+    }
+    elem.style.left = xpos + "px";
+    elem.style.top = ypos + "px";
+    var repeat = "moveElement('" + elementID + "'," + xpos + "," + ypos
+                + "," + interval + ")";
+    movement = setTimeout(repeat, interval);
+}
+
+function prepareSlideshow(){
+    var intro = document.getElementById("intro");
+    var slideshow = document.createElement("div");
+    slideshow.setAttribute("id", "slideshow");
+    var preview = document.createElement("img");
+    preview.setAttribute("src", "images/slideshow.gif");
+    preview.setAttribute("alt", "Preview images");
+    preview.setAttribute("id", "preview");
+    slideshow.appendChild(preview);
+    insertAfter(slideshow, intro);
+
+    var links = document.getElementsByTagName("a");
+    for(var i = 0; i < links.length; i++){
+        links[i].onmouseover = function() {
+            destination = this.getAttribute("href");
+            if(destination.indexOf("index.html") != -1){
+                moveElement("preview", 0, 0, 5);
+            }
+            if(destination.indexOf("about.html") != -1){
+                moveElement("preview", -150, 0, 5);
+            }
+            if(destination.indexOf("photos.html") != -1){
+                moveElement("preview", -300, 0, 5);
+            }
+            if(destination.indexOf("live.html") != -1){
+                moveElement("preview", -450, 0, 5);
+            }
+            if(destination.indexOf("contact.html") != -1){
+                moveElement("preview", -600, 0, 5);
+            }
+        }
+    }
+    addLoadEvent(prepareSlideshow);
+}
