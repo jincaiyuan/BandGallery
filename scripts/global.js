dd9ab5c26d23558ca485986dafefbba5a3964500
jincaiyuan <cywalker@163.com>
2017-06-09 12:29:40 +0800

    1. Fix bug that addLoadEvent function do not load function correctly 2. Finish moveElement and prepareSlideShow

--- scripts/global.js
+++ scripts/global.js
@@ -1,6 +1,6 @@
 function addLoadEvent(func){
     var oldLoad = window.onload;
-    if(typeof Window.onload != 'function'){
+    if(typeof window.onload != "function"){
         window.onload = func;
     }else {
         window.onload = function() {
@@ -46,31 +46,47 @@ function highlightPage() {
 }
 addLoadEvent(highlightPage);
 
-
+//For this function I need to do some research
 function moveElement(elementID, final_x, final_y, interval) {
     var elem = document.getElementById(elementID);
+    if(!elem){
+        return false;
+    }
+    if(elem.movement) {
+        clearTimeout(elem.movement);
+    }
+    if(!elem.style.left){
+        elem.style.left = "0px";
+    }
+    if(!elem.style.top){
+        elem.style.top = "0px";
+    }
     var xpos = parseInt(elem.style.left);
     var ypos = parseInt(elem.style.top);
     if(xpos == final_x && ypos == final_y){
         return true;
     }
     if(xpos < final_x){
-        xpos++;
+        var dist = Math.ceil((final_x - xpos)/10);
+        xpos += dist;
     }
     if(xpos > final_x){
-        xpos--;
+        var dist = Math.ceil((xpos - final_x)/10);
+        xpos -= dist;
     }
     if(ypos < final_y){
-        ypos++;
+        var dist = Math.ceil((final_y - ypos)/10);
+        ypos += dist;
     }
     if(ypos > final_y){
-        ypos--;
+        var dist = Math.ceil((ypos - final_y)/10);
+        ypos -= dist;
     }
     elem.style.left = xpos + "px";
     elem.style.top = ypos + "px";
-    var repeat = "moveElement('" + elementID + "'," + xpos + "," + ypos
+    var repeat = "moveElement('" + elementID + "'," + final_x + "," + final_y
                 + "," + interval + ")";
-    movement = setTimeout(repeat, interval);
+    elem.movement = setTimeout(repeat, interval);
 }
 
 function prepareSlideshow(){
@@ -84,7 +100,7 @@ function prepareSlideshow(){
     slideshow.appendChild(preview);
     insertAfter(slideshow, intro);
 
-    var links = document.getElementsByTagName("a");
+    var links = intro.getElementsByTagName("a");
     for(var i = 0; i < links.length; i++){
         links[i].onmouseover = function() {
             destination = this.getAttribute("href");
@@ -104,6 +120,6 @@ function prepareSlideshow(){
                 moveElement("preview", -600, 0, 5);
             }
         }
-    }
-    addLoadEvent(prepareSlideshow);
+    }  
 }
+addLoadEvent(prepareSlideshow);
